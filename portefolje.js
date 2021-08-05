27cf0eb56e0f4649b07c6c1a6f7e059611a2fe89
Bao Mark Nguyen <bao.m.nguyen@hiof.no>
2021-08-05 17:42:21 +0200

    edits

--- portefolje.js
+++ portefolje.js
@@ -29,11 +29,11 @@ function oppstart(){
 
     //h2 Fade
     if(percent <= 5){
-        document.getElementById("h2").style.opacity = "1";
+        document.getElementById("overskrift_2").style.opacity = "1";
    
    }
     else{
-        document.getElementById("h2").style.opacity = "0";
+        document.getElementById("overskrift_2").style.opacity = "0";
    }
 
     //linje Fade
@@ -44,9 +44,95 @@ function oppstart(){
     else{
         document.getElementById("line").style.opacity = "0";
    }
+
+    //overskrift 3
+    if(percent >= 20){
+        document.getElementById("overskrift_3").style.opacity = "1";
    
+   }
+    else{
+        document.getElementById("overskrift_3").style.opacity = "0";
+   }
+
+    //overskrift 3_msc
+    if(percent >= 20){
+        document.getElementById("overskrift_3_msc").style.opacity = "1";
+   
+   }
+    else{
+        document.getElementById("overskrift_3_msc").style.opacity = "0";
+   }
+
+    //bakgrunn
+    if(percent >= 20){
+    document.getElementById("container").style.backgroundColor = "black";
+    }
+    
+    else{
+        document.getElementById("container").style.backgroundColor = "white";
+    }
+
+    //vedlag 1
+    if(percent >= 20){
+        document.getElementById("vedlag_1").style.opacity = "1";
+   
+   }
+    else{
+        document.getElementById("vedlag_1").style.opacity = "0";
+   }
+
+   //overskrift 4
+   if(percent >= 40){
+    document.getElementById("overskrift_4").style.opacity = "1";
+
+    }
+    else{
+        document.getElementById("overskrift_4").style.opacity = "0";
+    }
+
+    //overskrift 4_msc
+    if(percent >= 40){
+        document.getElementById("overskrift_4_msc").style.opacity = "1";
+
+    }
+    else{
+        document.getElementById("overskrift_4_msc").style.opacity = "0";
+    }
+
+    //vedlag 2
+    if(percent >= 40){
+        document.getElementById("vedlag_2").style.opacity = "1";
+   
+   }
+    else{
+        document.getElementById("vedlag_2").style.opacity = "0";
+   }
+
+   //overskrift 4 --- fade ut
+   if(percent >= 80){
+    document.getElementById("overskrift_4").style.opacity = "0";
+
+    }
 
 
+    //overskrift 4_msc --- fade ut
+    if(percent >= 80){
+        document.getElementById("overskrift_4_msc").style.opacity = "0";
+
+    }
+
+
+    //vedlag 2 --- fade ut
+    if(percent >= 80){
+        document.getElementById("vedlag_2").style.opacity = "0";
+   
+   }
+
+   //bakgrunn --- fade ut til hvit
+   if(percent >= 80){
+    document.getElementById("container").style.backgroundColor = "white";
+    }
+    
 
     })
 
